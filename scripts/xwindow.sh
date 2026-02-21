6a6bb2148afc75e2228caf200ddca72c07ed812e
Draco <75392428+Draco-GitHub@users.noreply.github.com>
2026-02-21 19:36:53 +0000

    Refactor xwindow.sh to handle active window check

--- scripts/xwindow.sh
+++ scripts/xwindow.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
 
 active_window=$(xprop -root _NET_ACTIVE_WINDOW | awk '{print $5}')
-wm_class=$(xprop -id "$active_window" WM_CLASS)
+
+# If no active window
+if [[ -z "$active_window" || "$active_window" == "0x0" ]]; then
+    echo ""
+    exit 0
+fi
+
+wm_class=$(xprop -id "$active_window" WM_CLASS 2>/dev/null)
+title=$(xprop -id "$active_window" WM_NAME 2>/dev/null | cut -d '"' -f2)
 
 instance=$(echo "$wm_class" | cut -d '"' -f2)
 class=$(echo "$wm_class" | cut -d '"' -f4)
 
-title=$(xprop -id "$active_window" WM_NAME | cut -d '"' -f2)
-
-if [[ "${instance,,}" == "ghostty" ]]; then
+if [[ "${instance,,}" == "alacritty" ]]; then
     echo " $title"
 
 elif [[ "${class,,}" == "librewolf" ]]; then
     echo "󰈹 $title"
+
+elif [[ "${class,,}" == "discord" ]]; then
+    echo "  $title"
 
 else
     echo "$title"
